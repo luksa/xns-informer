cb0a59b8b45de6dba7443a019a19c14615bff49a
Marko Lukša <marko.luksa@gmail.com>
2023-01-17 10:12:12 +0100

    OSSM-3373 Add OpenShift Routes to KubeInformerFactory

--- dependencies.go
+++ dependencies.go
@@ -1,9 +1,12 @@
 package xnsinformer
 
 import (
-	// xxx
+	// import gateway API so we can generate the informer factory
 	_ "sigs.k8s.io/gateway-api/conformance/utils/suite"
 
-	// xxx
+	// import istio API to generate the istio informer factory
 	_ "istio.io/client-go/pkg/clientset/versioned"
+
+	// import openshift route API
+	_ "github.com/openshift/api/route/v1"
 )
